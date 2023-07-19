126eda1b8ba3fdd148967316193faeddeda21d94
Oxidra <dantelezzo@gmail.com>
2023-07-19 15:02:33 +0200

    imported footer & fixed footer background img

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,121 @@
+<footer>
+    <nav class="container">
+            <div class="first-block">
+                <div id="dc-comics">
+                    <h2>DC COMICS</h2>
+                    <ul>
+                        <li>
+                            <a href="#">placeholder</a>
+                        </li>
+                        <li>
+                            <a href="#">placeholder</a>
+                        </li>
+                        <li>
+                            <a href="#">placeholder</a>
+                        </li>
+                        <li>
+                            <a href="#">placeholder</a>
+                        </li>
+                        <li>
+                            <a href="#">placeholder</a>
+                        </li>
+                        <li>
+                            <a href="#">placeholder</a>
+                        </li>
+                    </ul>
+                </div>
+    
+                <div id="shop">
+                    <h2>SHOP</h2>
+                    <ul>
+                        <li>
+                            <a href="#">placeholder</a>
+                        </li>
+                        <li>
+                            <a href="#">placeholder</a>
+                        </li>
+                        <li>
+                            <a href="#">placeholder</a>
+                        </li>
+                        <li>
+                            <a href="#">placeholder</a>
+                        </li>
+                        <li>
+                            <a href="#">placeholder</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="second-block">
+                <div id="dc">
+                    <h2>DC</h2>
+                    <ul>
+                        <li>
+                            <a href="#">placeholder</a>
+                        </li>
+                        <li>
+                            <a href="#">placeholder</a>
+                        </li>
+                        <li>
+                            <a href="#">placeholder</a>
+                        </li>
+                        <li>
+                            <a href="#">placeholder</a>
+                        </li>
+                        <li>
+                            <a href="#">placeholder</a>
+                        </li>
+                    </ul>
+                </div>
+    
+                <div id="sites">
+                    <h2>SITES</h2>
+                    <ul>
+                        <li>
+                            <a href="#">placeholder</a>
+                        </li>
+                        <li>
+                            <a href="#">placeholder</a>
+                        </li>
+                        <li>
+                            <a href="#">placeholder</a>
+                        </li>
+                        <li>
+                            <a href="#">placeholder</a>
+                        </li>
+                        <li>
+                            <a href="#">placeholder</a>
+                        </li>
+                        <li>
+                            <a href="#">placeholder</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+
+            <div class="logo">
+                <img src="{{ Vite::asset('resources/img/dc-logo-bg.png')}}" alt="dc logo">
+            </div>
+    </nav>
+
+    <div class="bottom-footer">
+        <div class="container">
+            <button>SIGN-UP NOW!</button>
+
+            <div class="socials">
+                <ul>
+                    <li><a href="#">FOLLOW US</a></li>
+                    <li><a href="#"><img src="{{ Vite::asset('resources/img/footer-facebook.png')}}" alt="facebook logo"></a></li>
+                    <li><a href="#"><img src="{{ Vite::asset('resources/img/footer-twitter.png')}}" alt="twitter logo"></a></li>
+                    <li><a href="#"><img src="{{ Vite::asset('resources/img/footer-youtube.png')}}" alt="youtube logo"></a></li>
+                    <li><a href="#"><img src="{{ Vite::asset('resources/img/footer-pinterest.png')}}" alt="pinterest logo"></a></li>
+                    <li><a href="#"><img src="{{ Vite::asset('resources/img/footer-periscope.png')}}" alt="periscope logo"></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="bottom-bar"></div>
+</footer>
