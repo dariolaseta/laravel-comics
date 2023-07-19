e48a0cde974d0f4a3a8bb413fe6f773dcf5665e2
Oxidra <dantelezzo@gmail.com>
2023-07-19 14:57:16 +0200

    imported main

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,5 +3,20 @@
 @section('title', 'Hello World')
 
 @section('main-content')
+<main>
+    <div class="jumbo">
+        <img src="{{ Vite::asset('resources/img/jumbotron.jpg')}}" alt="jumbo img">
+    </div>
 
+    <div class="container">
+        <button class="top-button">CURRENT SERIES</button>
+
+        <div class="card">
+            <img src="" alt="test">
+            <p>test</p>
+        </div>
+
+        <button class="load-more">LOAD MORE</button>
+    </div>
+</main>
 @endsection
